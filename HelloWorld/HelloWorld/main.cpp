eb58072fcf724ceb8fa922595db9fe07c6a465be
Isabelle Zelaya <isabazal@umich.edu>
2018-03-27 23:00:07 -0400

    added a sum function

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int sum(int x, int y)
+{
+	return x + y;
+}
+
 int main()
 {
-	cout << "Hello World" << endl;
+	cout << "Hello World " << sum(4, 5) << endl;
 	return 0;
 }
