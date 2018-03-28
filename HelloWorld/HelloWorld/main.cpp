589c0798fdc1ffe7e8545b0aaaf9338d878b9e01
Isabelle Zelaya <isabazal@umich.edu>
2018-03-27 23:10:38 -0400

    added subtract

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -6,8 +6,13 @@ int sum(int x, int y)
 	return x + y;
 }
 
+int subtract(int x, int y)
+{
+	return x - y;
+}
+
 int main()
 {
-	cout << "Hello World " << sum(4, 5) << endl;
+	cout << "Hello World " << subtract(4, 5) << endl;
 	return 0;
 }
